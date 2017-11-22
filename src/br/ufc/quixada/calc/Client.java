fcbaf79426549d5e60fe73db50a8326edcb74adb
Iarlen <iarlensilva775@gmail.com>
2017-11-22 09:46:25 -0300

    Implementando os testes

--- src/br/ufc/quixada/calc/Client.java
+++ src/br/ufc/quixada/calc/Client.java
@@ -16,6 +16,15 @@ public class Client {
 			System.out.println("Resposta: " + response);
 			double resultado = proxy.mul(20, 35);
 			System.out.println("Mul 20 * 35 = " + resultado);
+			
+			double resultSoma = proxy.som(20, 35);
+			System.out.println("Som 20 + 35 = " + resultSoma);
+			
+			double resultSub = proxy.sub(20, 35);
+			System.out.println("Sub 20 - 35 = " + resultSub);
+			
+			double resultDiv = proxy.div(20, 35);
+			System.out.println("Div 20 / 35 = " + resultDiv);
 
 		} catch (Exception e) {
 			System.err.println("Client exception: " + e.toString());
